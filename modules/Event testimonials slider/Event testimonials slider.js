4b09321034e78bda729c6d051654e92d7dec19a5
kacperwalter <kacwal@poczta.fm>
2024-12-19 11:34:11 +0100

    Improved logic

--- modules/Event testimonials slider/Event testimonials slider.js
+++ modules/Event testimonials slider/Event testimonials slider.js
@@ -31,11 +31,15 @@ class EventTestimonialsSlider {
 
   addListeners() {
     if (this.prevButton) {
-      this.prevButton.addEventListener("click", () => this.showPrevious());
+      this.prevButton.addEventListener("click", () => {
+        this.showPrevious();
+      });
     }
 
     if (this.nextButton) {
-      this.nextButton.addEventListener("click", () => this.showNext());
+      this.nextButton.addEventListener("click", () => {
+        this.showNext();
+      });
     }
   }
 
@@ -60,14 +64,43 @@ class EventTestimonialsSlider {
   }
 
   updateSliderPosition() {
-    const offset = -this.currentIndex * 70; // Calculate offset based on currentIndex
+    // Adjust the offset if needed. Currently each slide is 100% width, 
+    // so translating by 100% per slide might be what you want.
+    const offset = -this.currentIndex * 100;
     this.slider.style.transform = `translateX(${offset}%)`;
   }
 
   updateProgressBar() {
     if (this.progressBar) {
-      // const progress = ((this.currentIndex + 1) / this.testimonialCount) * 100;
-      // this.progressBar.style.width = `${progress}%`;
+      const currentLabel = this.progressBar.querySelector("[data-progress-bar-current]");
+      const totalLabel = this.progressBar.querySelector("[data-progress-bar-all]");
+      const filledLine = this.progressBar.querySelector(".progress-bar__line-filled");
+      const dot = this.progressBar.querySelector(".progress-bar__dot");
+  
+      // Update numbers
+      if (currentLabel) currentLabel.textContent = String(this.currentIndex + 1).padStart(2, "0");
+      if (totalLabel) totalLabel.textContent = String(this.testimonialCount).padStart(2, "0");
+  
+      // Calculate progress as a percentage of how far along we are
+      const progress = this.testimonialCount > 1
+        ? (this.currentIndex / (this.testimonialCount - 1)) * 100
+        : 0;
+  
+      // Update the line and dot positions
+      if (filledLine) {
+        filledLine.style.width = progress + "%";
+      }
+  
+      if (dot) {
+        dot.style.left = progress + "%";
+      }
+  
+      // Change the colors when on the last slide
+      if (this.currentIndex === this.testimonialCount - 1) {
+        if (totalLabel) totalLabel.style.color = "var(--color-brand)";
+      } else {
+        if (totalLabel) totalLabel.style.color = "var(--color-brand-light)";
+      }
     }
   }
 }
