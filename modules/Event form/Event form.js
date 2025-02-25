6d7fa7518ea52dfbdbe3854ab87b04404d238c3c
kacperwalter <kacper.walter@gmail.com>
2025-02-25 13:06:59 +0100

    Add logic for Event form

--- modules/Event form/Event form.js
+++ modules/Event form/Event form.js
@@ -1,29 +1,34 @@
-const targetSelector = '.submitted-message'
+const targetSelector = '.submitted-message';
 
 const observerCallback = (mutationsList, observer) => {
     for (const mutation of mutationsList) {
         if (mutation.type === 'childList') {
-            const targetElement = document.querySelector(targetSelector)
+            const targetElement = document.querySelector(targetSelector);
 
             if (targetElement) {
-                const headingToRemove = document.querySelector(["[data-form-heading]"])
-                const elementToOffset = document.querySelector("[data-event-partner]")
-                
+                const headingToRemove = document.querySelector("[data-form-heading]");
+                const elementToOffset = document.querySelector("[data-event-partner]");
+                const eventForm = document.querySelector(".event-form__form");
+
                 if (headingToRemove) {
-                  headingToRemove.remove()
+                    headingToRemove.remove();
                 }
 
                 if (elementToOffset) {
-                    elementToOffset.style.marginTop = "10rem"
-              }
+                    elementToOffset.style.marginTop = "10rem";
+                }
+                
+                if (eventForm) {
+                    eventForm.style.backgroundColor = "transparent";
+                    eventForm.style.boxShadow = "none";
+                }
 
-                observer.disconnect()
-                break
+                observer.disconnect();
+                break;
             }
         }
     }
-}
+};
 
-const observer = new MutationObserver(observerCallback)
-
-observer.observe(document.body, { childList: true, subtree: true })
+const observer = new MutationObserver(observerCallback);
+observer.observe(document.body, { childList: true, subtree: true });
